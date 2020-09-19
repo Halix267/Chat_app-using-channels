6fa997b19ee99be90035aea29710025fbbe364c5
HALIX <harshitdhyani575@gmail.com>
2020-09-19 14:41:04 +0530

    improving

--- Chat_application/mysite/mysite/settings.py
+++ Chat_application/mysite/mysite/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,6 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 ASGI_APPLICATION='mysite.routing.application'
 
